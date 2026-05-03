17a3288044b5d4a11fdbfe069ff48a7c649218c0
Loc Tran <loctranhoang@gmail.com>
2026-05-03 20:07:02 +0700

    feat: apply DevFlow changes (AUTH-164)

--- src/test/java/com/az/auth/AuthServiceApplicationTests.java
+++ src/test/java/com/az/auth/AuthServiceApplicationTests.java
@@ -2,19 +2,14 @@ package com.az.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-public class AuthServiceApplicationTests {
-
-    @Autowired
-    private ApplicationContext context;
+class AuthServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        assertThat(context).isNotNull();
+        assertNotNull(); // Ensure the application context loads
     }
 }
