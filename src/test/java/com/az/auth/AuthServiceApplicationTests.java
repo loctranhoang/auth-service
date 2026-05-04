b2114e8f24bf9ff9a6a41ecd59f8b0a1e3418101
Loc Tran <loctranhoang@gmail.com>
2026-05-04 16:50:47 +0700

    feat: apply DevFlow changes (AUTH-172)

--- src/test/java/com/az/auth/AuthServiceApplicationTests.java
+++ src/test/java/com/az/auth/AuthServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.az.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AuthServiceApplicationTests {
 
     @Test
