bc79ea0b0a138903d657620e791ff9cc23071a98
Loc Tran <loctranhoang@gmail.com>
2026-05-10 14:44:24 +0700

    feat: apply DevFlow changes (AUTH-188)

--- src/test/java/com/az/auth/AuthServiceApplicationTests.java
+++ src/test/java/com/az/auth/AuthServiceApplicationTests.java
@@ -3,22 +3,13 @@ package com.az.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("local")
 class AuthServiceApplicationTests {
 
-    @Autowired(required = false)
-    private TestRestTemplate restTemplate;
-
     @Test
     void contextLoads() {
         // This test will pass if the application context loads successfully
-        assertThat(true).isTrue();
     }
 }
