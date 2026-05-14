9b310e31b38d9bfc6ac8da8814a03df117203449
Loc Tran <loctranhoang@gmail.com>
2026-05-14 11:29:21 +0700

    feat: apply DevFlow changes (AUTH-193)

--- src/main/java/com/az/auth/AuthServiceApplication.java
+++ src/main/java/com/az/auth/AuthServiceApplication.java
@@ -3,6 +3,17 @@ package com.az.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// For OpenAPI standard annotations
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Auth Service API",
+        version = "1.0",
+        description = "API documentation for the Auth Service."
+    )
+)
 @SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
