fbaf18b4123f2280872d4e5dc1d595a7d97d6d84
Loc Tran <loctranhoang@gmail.com>
2026-05-21 11:43:31 +0700

    feat: apply DevFlow changes (AUTH-223)

---
+++ src/test/java/com/az/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test will fail if the Spring application context cannot start
+    }
+}
