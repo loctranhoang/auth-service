2601eb64c2d65f2ead7f8025131a0a8716ae37e9
Loc Tran <loctranhoang@gmail.com>
2026-05-26 21:38:51 +0700

    feat: reconcile DevFlow child delivery (AUTH-232)

---
+++ src/test/java/com/az/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Context load test to verify that the Spring application context loads successfully
+    }
+}
